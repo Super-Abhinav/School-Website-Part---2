bf073a920f9b1f09c9a8ada1f9c773260c27f7be
Abhinav Anand <85018022+Super-Abhinav@users.noreply.github.com>
2021-08-07 21:53:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function btn() {
+    document.getElementById("dd").innerHTML = "Takshila Educational Society laid the foundation of veritable learning in Patna by establishing Delhi Public School in the year 1998 in a collaborative effort with the Delhi Public Schools Society, which is one of the largest chains of CBSE schools in India. DPS Patna is affiliated to CBSE, New Delhi and offers schooling from Nursery to Class XII. The last twenty three years have been a journey where perseverance and progress have been the cornerstones. Today it can proudly claim to be one of the most sought after schools in Eastern India.Far from the bustling urban environs, DPS Patna is situated near the Army Cantonment at Danapur in village Chandmari. A marvel of modern architecture, the campus contains well-lit airy classrooms, outdoor theatre, performing art block, cafeteria, spacious courtyards for indoor games, and beautifully landscaped lawns."
+}
+
+
